6147f5c969f451d1b2c914c53d53995cbd819b98
Max Base <MaxBaseCode@Gmail.Com>
2019-03-09 01:59:45 +0330

    Update hashmap.c

--- source/hashmap.c
+++ source/hashmap.c
@@ -1,1 +1,10 @@
-
+/**
+*
+* @Name : HashMap.c
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-03-09
+* @Released under : https://github.com/BaseMax/HashMapC/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/HashMapC
+*
+**/
