60b95a30a14d88935a5692c93893eded1ac77324
Max Base <MaxBaseCode@Gmail.Com>
2019-03-09 08:40:23 +0330

    Update hashmap.c

--- test/hashmap.c
+++ test/hashmap.c
@@ -10,37 +10,9 @@
 **/
 #include <stdio.>
 #include <stdlib.h>
+#include <assert.h>
 #include "../source/hashmap.h"
 
 int main() {
-	HashMap *array=malloc(sizeof(HashMap));
-	HashInit(array);
-
-	PairValue *value=malloc(sizeof(PairValue));
-	value->key="name";
-	value->value="test";
-	HashAdd(array,value);
-	// HashDeleteIndex(array,0);
-	// PairValue *get=HashGetIndex(array,0);
-	// PairValue *get=HashGetKey(array,"name");
-	// PairValue *get=HashGetValue(array,"test");
-	PairValue *values=malloc(sizeof(PairValue));
-	values->key="name";
-	values->value="test";
-	// values->key="new";
-	PairValue *get=HashGetEquals(array,values);
-	printf("==>%s,%s\n",get->key,get->value);
-	// printf("====>%s\n",array->data[0]->value);
-	HashAdd(array,value);
-	HashAdd(array,value);
-	HashAdd(array,value);
-
-	// printf("==>%d\n",array->length);
-	// for(size_t index=0;index<array->length;index++)
-	// {
-	// 	// printf("==>%s\n",array->data[index]);
-	// 	printf("==>%s : %s\n",array->data[index]->key,array->data[index]->value);
-	// }
-
 	return 0;
 }
