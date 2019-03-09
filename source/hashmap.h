ed0298eb074ad4db4765bf15f7294c6e58f54ce7
Max Base <MaxBaseCode@Gmail.Com>
2019-03-09 08:24:14 +0330

    Update hashmap.h

--- source/hashmap.h
+++ source/hashmap.h
@@ -8,3 +8,15 @@
 * @Repository : https://github.com/BaseMax/HashMapC
 *
 **/
+void HashInit(HashMap *array);
+void HashAdd(HashMap *array,PairValue *value);
+void HashAppend(HashMap *array,HashMap *append);
+void HashFreeIndex(HashMap *array,size_t index);
+void HashFree(HashMap *array);
+int HashDeleteIndex(HashMap *array,size_t index);
+void HashDelete(HashMap *array);
+PairValue *HashGetIndex(HashMap *array,size_t index);
+PairValue *HashGetKey(HashMap *array,void *key);
+PairValue *HashGetValue(HashMap *array,void *value);
+PairValue *HashGetEquals(HashMap *array,PairValue *pair);
+PairValue *HashGetEqual(HashMap *array,void *key,void *value);
